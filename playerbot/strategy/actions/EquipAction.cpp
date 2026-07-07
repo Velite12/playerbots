e17bd4ab195cf7556149b8b0d9f99931d8a0996f
Velite12 <45871932+Velite12@users.noreply.github.com>
2026-07-07 06:00:48 -0400

    Update EquipAction.cpp

--- playerbot/strategy/actions/EquipAction.cpp
+++ playerbot/strategy/actions/EquipAction.cpp
@@ -162,9 +162,6 @@ void EquipAction::EquipItemsToSlot(Player* requester, ItemIds ids, uint8 targetS
             Item* item = *items.begin();
             
             EquipItemToSlot(requester, *items.begin(), targetSlot);
-            // auto enchant if cheat is turned on
-            if (sPlayerbotAIConfig.autoEnchantUpgradeLoot && IsEnchantable(item) && !item->GetEnchantmentId(EnchantmentSlot(0)))
-                EnchantItem(item);
         }
     }
 }
